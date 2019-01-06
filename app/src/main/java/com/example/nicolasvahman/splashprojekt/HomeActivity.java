e0366326c15e2b98f10fdb3d706ef14053c55b54
nicolasvahman <kavehv@live.dk>
2019-01-06 10:13:30 +0100

    Listen virker nu, der kan gættes på ordne på listen. Der er tilføjet menu på forsiden foreløbigt.

--- app/src/main/java/com/example/nicolasvahman/splashprojekt/HomeActivity.java
+++ app/src/main/java/com/example/nicolasvahman/splashprojekt/HomeActivity.java
@@ -3,6 +3,9 @@ package com.example.nicolasvahman.splashprojekt;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -30,7 +33,7 @@ public class HomeActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        getSupportActionBar().hide();
+
 
 
         anbefaltilven = (Button) findViewById(R.id.anbefal);
@@ -171,7 +174,30 @@ public class HomeActivity extends AppCompatActivity  {
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        switch (item.getItemId()){
 
+            case R.id.item1:
+                Intent startside = new Intent( HomeActivity.this, HomeActivity.class);
+                startActivity(startside);
+
+            case R.id.item2:
+                Intent gættelegen = new Intent(HomeActivity.this, guessGame.class);
+                startActivity(gættelegen);
+            case R.id.item3:
+                Intent highscore = new Intent(HomeActivity.this, Highscore.class);
+                startActivity(highscore);
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/example/nicolasvahman/splashprojekt/guess2.java
+++ app/src/main/java/com/example/nicolasvahman/splashprojekt/guess2.java
@@ -1,8 +1,9 @@
 package com.example.nicolasvahman.splashprojekt;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,43 +14,48 @@ public class guess2 extends AppCompatActivity {
     EditText gættelade;
     TextView vinderen;
     Button okknap;
+    TextView taber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess2);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(guess2.this, R.raw.klap);
 
         final Intent secondIntent = getIntent();
         String besked = "Du har valgt følgende ord: " + secondIntent.getStringExtra("et");
 
-        TextView infoM = (TextView) findViewById(R.id.guessssss);
+        TextView infoM = findViewById(R.id.guessssss);
         infoM.setText(besked);
 
         vinderen = findViewById(R.id.vundetET);
+        gættelade = findViewById(R.id.guesfelt);
+        taber = findViewById(R.id.taberfelt);
 
         okknap = findViewById(R.id.okknap);
 
         okknap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-            }
-        });
-
-
-
-
-        gættelade = findViewById(R.id.guesfelt);
-
-        gættelade.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(gættelade.equals(  secondIntent.getStringExtra("et"))){
-                    vinderen.setText("Du har gættet rigtigt");
+                if(secondIntent.getStringExtra("et").equals(gættelade.getText().toString())) {
+                    vinderen.setText("Du har gættet rigtigt!");
+                    mediaPlayer.start();
 
                 }
+
+                else{
+                    taber.setText("Du har gættet forkert, prøv igen");
+                    taber.setText("");
+                    gættelade.setText("");
+                }
+
             }
         });
+
+
+
+
+
 
 
 
